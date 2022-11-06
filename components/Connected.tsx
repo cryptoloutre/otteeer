c88940e4b95da59ea1e78fd8348a5aeb31e1d5e2
cryptoloutre <miningcryptomonnaie@gmail.com>
2022-11-06 15:39:26 +0100

    init

---
+++ components/Connected.tsx
@@ -0,0 +1,54 @@
+import { FC } from "react"
+import {
+  Button,
+  Container,
+  Heading,
+  HStack,
+  Text,
+  VStack,
+  Image,
+} from "@chakra-ui/react"
+import { ArrowForwardIcon } from "@chakra-ui/icons"
+
+const Connected: FC = () => {
+  return (
+    <VStack spacing={20}>
+      <Container>
+        <VStack spacing={8}>
+          <Heading
+            color="white"
+            as="h1"
+            size="2xl"
+            noOfLines={2}
+            textAlign="center"
+          >
+            Welcome Otteerr Loveeer.
+          </Heading>
+
+          <Text color="bodyText" fontSize="xl" textAlign="center">
+            Each otterrr is randomly generated and can be staked to receive
+            <Text as="b"> $OTTY</Text> Use your <Text as="b"> $OTTY</Text> to
+            upgrade your otterrr and receive perks within the community!
+          </Text>
+        </VStack>
+      </Container>
+
+      <HStack spacing={10}>
+        <Image src="avatar1_.png" alt="" />
+        <Image src="avatar2_.png" alt="" />
+        <Image src="avatar3_.png" alt="" />
+        <Image src="avatar4_.png" alt="" />
+        <Image src="avatar5_.png" alt="" />
+      </HStack>
+
+      <Button bgColor="accent" color="white" maxW="380px">
+        <HStack>
+          <Text>mint buildoor</Text>
+          <ArrowForwardIcon />
+        </HStack>
+      </Button>
+    </VStack>
+  )
+}
+
+export default Connected
